99f60dd76386a9d4d6b3e4f552eb0d17c2f9645a
单应青 <462548187@qq.com>
2020-03-31 01:12:27 +0800

    1

---
+++ test.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    @Author         :  Sandy
+    @Version        :  
+  ----------------------------------
+    @File           :  test.py.py
+    @Description    :  
+    @CreateTime     :  2020/3/30 10:58 下午
+    @Software       :  PyCharm
+  -----------------------------------
+    @ModifyTime     : 
+"""
+# 整数相除，等到浮点数
+print(5 / 2)  # 2.5
+
+# 整数省去小数位
+print(5 // 2)  # 2
+
+# 求余数
+print(5 % 2)  # 1
+
+# 5的3次方
+print(5 ** 3)  # 125
+
+# 比较大小，返回 布尔类型
+print(5 > 3)  # True
+
+# python支持连续比较，返回 布尔类型
+print(20 > 10 > 5)  # False
+
+# 比较大小结果与运算，返回 布尔类型
+print(20 > 10 and 10 < 5)  # False
+
+# 比较大小结果或运算，返回 布尔类型
+print(20 > 10 or 10 > 5)  # True
+
+# 比较大小结果取非运算，返回 布尔类型
+print(not 20 > 10)  # False
